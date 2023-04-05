62ecb5c6df80f44d209c6ba222abb7027a27ec64
Lucas Martins Efrens <lucas_efrens@outlook.com>
2023-04-05 15:31:05 -0300

    revisando funções

---
+++ 05funcoes/script.js
@@ -0,0 +1,29 @@
+function primeiraFuncao() {
+    
+    console.log("Isso é uma função");
+}
+
+primeiraFuncao();
+
+function dizerNome(nome) {
+    
+    console.log(`O nome é: ${nome}`);
+}
+
+dizerNome("Lucas");
+dizerNome("Tiago");
+dizerNome("Felipe");
+
+var nomeDoBancoDeDados = "João";
+
+dizerNome(nomeDoBancoDeDados);
+
+function soma(a, b) {
+    return a + b;
+}
+
+var somaUm = soma(1, 1);
+
+console.log(somaUm);
+
+console.log(soma(2, 2));
