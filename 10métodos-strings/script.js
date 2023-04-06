0bbdd20fe87f920431f93b44181c5cf1c4395ae8
Lucas Martins Efrens <lucas_efrens@outlook.com>
2023-04-06 09:32:07 -0300

    exercícios métodos númericos

---
+++ 10métodos-strings/script.js
@@ -0,0 +1,22 @@
+// parseFloat 10.0
+
+console.log(parseFloat("10.99"));
+
+console.log(Number.parseFloat("10.99"));
+
+// parseInt
+
+console.log(parseInt("16.96"));
+
+console.log(23.81646231.toFixed(1))
+
+// isNaN
+
+console.log(isNaN("teste"));
+console.log(isNaN(12));
+console.log(isNaN("14"));
+
+// MAX_VALUE e MIN_VALUE
+
+console.log(Number.MIN_VALUE);
+console.log(Number.MAX_VALUE);
