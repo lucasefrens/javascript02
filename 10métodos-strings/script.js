b249ec0f482e953c018febebee45df5eca40509d
Lucas Martins Efrens <lucas_efrens@outlook.com>
2023-04-06 09:41:23 -0300

    métodos strings

--- 10métodos-strings/script.js
+++ 10métodos-strings/script.js
@@ -1,22 +1,5 @@
-// parseFloat 10.0
+var nome = "Lucas";
 
-console.log(parseFloat("10.99"));
+console.log(nome.length);
 
-console.log(Number.parseFloat("10.99"));
-
-// parseInt
-
-console.log(parseInt("16.96"));
-
-console.log(23.81646231.toFixed(1))
-
-// isNaN
-
-console.log(isNaN("teste"));
-console.log(isNaN(12));
-console.log(isNaN("14"));
-
-// MAX_VALUE e MIN_VALUE
-
-console.log(Number.MIN_VALUE);
-console.log(Number.MAX_VALUE);
+indexOf
