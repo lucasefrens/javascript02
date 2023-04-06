ceea5cd8b9b9dec6e27588d9f8d13ad810818558
Lucas Martins Efrens <lucas_efrens@outlook.com>
2023-04-06 09:51:59 -0300

    exercício métodos strings

--- 10métodos-strings/script.js
+++ 10métodos-strings/script.js
@@ -1,5 +1,25 @@
+//length
+
 var nome = "Lucas";
 
 console.log(nome.length);
 
-indexOf
+// indexOf
+
+console.log(nome[2]);
+
+var frase = "O rato roeu a roupa do rei de Roma";
+
+console.log(frase.indexOf("roeu"));
+
+// slice
+
+var roeu = frase.slice(7, 11);
+
+console.log(roeu);
+
+// replace
+
+var novaFrase = frase.replace("roeu", "teste");
+
+console.log(novaFrase);
