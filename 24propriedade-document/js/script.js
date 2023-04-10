e70a3ce651ce7a47bd1d2a7770203f7421a1a61b
Lucas Martins Efrens <lucas_efrens@outlook.com>
2023-04-10 15:54:54 -0300

    Algumas popriedades do document

---
+++ 24propriedade-document/js/script.js
@@ -0,0 +1,16 @@
+// propriedades document
+console.log(document.body);
+
+console.log(document.head);
+
+console.log(document.links[0]);
+
+document.links[0].textContent = 'Twitter';
+
+console.log(document.URL);
+
+console.log(document.title);
+
+document.title = 'TÍTULO ALTERADO COM JS'
+
+console.log(document.title)
