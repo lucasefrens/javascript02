9638c21f3c3a574fb9ac8efc933859569419f4b1
Lucas Martins Efrens <lucas_efrens@outlook.com>
2023-04-11 15:22:50 -0300

    eventos mouseover e mouseout

---
+++ 30evento-mouseover-mouseout/js/script.js
@@ -0,0 +1,32 @@
+// evento de mouseover
+var title = document.querySelector('#title');
+
+title.addEventListener('mouseover', function() {
+
+    this.style.backgroundColor = 'yellow';
+
+});
+
+// evento mouseout
+title.addEventListener('mouseout', function() {
+
+    this.style.backgroundColor = 'white'
+
+});
+
+// afetar outros elementos com mouseover
+var subtitle = document.querySelector('.subtitle');
+
+subtitle.addEventListener('mouseover', function() {
+
+    var legenda = document.querySelector('#legenda');
+    
+    legenda.classList.remove('hide');
+})
+
+subtitle.addEventListener('mouseout', function() {
+
+    var legenda = document.querySelector('#legenda');
+    
+    legenda.classList.add('hide');
+})
