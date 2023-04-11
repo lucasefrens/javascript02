72e2395d39e92a26b6147506e4ac418b5593610d
Lucas Martins Efrens <lucas_efrens@outlook.com>
2023-04-11 15:09:48 -0300

    eventos onclick e dblclick

---
+++ 29evento-onclick/js/script.js
@@ -0,0 +1,35 @@
+// inserir click
+var btn = document.querySelector('#btn');
+
+console.log(btn);
+
+btn.addEventListener('click', function() {
+
+    console.log('clicou');
+
+    console.log(this)
+
+    this.style.color = 'red';
+
+});
+
+
+// clique afetando outros elementos
+var title = document.querySelector('#title');
+
+title.addEventListener('click', function() {
+
+    var subtitle = document.querySelector('.subtitle');
+
+    subtitle.style.display = 'none';
+
+});
+
+// double click
+var subtitle = document.querySelector('.subtitle');
+
+subtitle.addEventListener('dblclick', function() {
+    
+    console.log('click duplo!')
+
+})
