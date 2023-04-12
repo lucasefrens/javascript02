ce356389e09042b5372889d2a26d45d1528f4ab6
Lucas Martins Efrens <lucas_efrens@outlook.com>
2023-04-12 10:55:35 -0300

    eventos keydown e keyup

---
+++ 31evento-keydown-keyup/js/script.js
@@ -0,0 +1,16 @@
+// keydown
+document.addEventListener('keydown', function(event) {
+    
+    if (event.key === 'Enter') {
+        console.log('Apertou o ENTER!');
+    }
+
+});
+
+// keyup
+document.addEventListener('keyup', function(e) {
+    
+    if(e.key === 'Enter') {
+        console.log('Soltou o Enter')
+    }
+})
